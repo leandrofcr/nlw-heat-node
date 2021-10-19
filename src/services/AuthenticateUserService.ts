288cb11b7b66aad31afc27482caeacd6ee4e61e1
Leandro Reis <lleandrofr@gmail.com>
2021-10-19 10:54:36 -0300

    feat: get user information from github when logging in

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,11 +1,23 @@
 import 'dotenv/config';
 import axios from 'axios';
 
+
+interface IAccessTokenResponse {
+  access_token: string
+}
+
+interface IUserResponse {
+  id: number,
+  name: string
+  login: string,
+  avatar_url: string,
+}
+
 class AuthenticateUserUserService {
   async execute(code: string) {
     const url = 'https://github.com/login/oauth/access_token';
 
-    const response = await axios.post(url, null, {
+    const { data: accessTokenResponse } = await axios.post<IAccessTokenResponse>(url, null, {
       params: {
         client_id: process.env.GITHUB_CLIENT_ID,
         client_secret: process.env.GITHUB_CLIENT_SECRET,
@@ -13,6 +25,12 @@ class AuthenticateUserUserService {
       },
       headers: {
         Accept: 'application/json',
+      },
+    });
+
+    const response = await axios.get<IUserResponse>('https://api.github.com/user',{
+      headers: {
+        authorization: `Bearer ${accessTokenResponse.access_token}`,
       },
     });
 
