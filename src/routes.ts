dca00daca82dae6ed398a5a965126464ae4b741e
Leandro Reis <lleandrofr@gmail.com>
2021-10-19 12:44:24 -0300

    feat: creates a new endpoint POST /messages
    This endpoint allows a logged in user to create and save a message to
    the database.

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import { AuthenticateUserController } from "./Controller/AuthenticateUserController";
+import { CreateMessageController } from "./Controller/CreateMessageController";
+import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
 
 const router = Router();
 
 router.post('/authenticate', new AuthenticateUserController().handle)
 
+router.post(
+  '/messages',
+  ensureAuthenticated,
+  new CreateMessageController().handle
+);
 
 export { router }
